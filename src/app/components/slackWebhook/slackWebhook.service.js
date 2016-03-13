fe5845270c3defd96e1a8960e156b602b50fb254
Ken Crocken <kcrocken@gmail.com>
2016-03-13 12:24:41 -0400

    service as https

--- src/app/components/slackWebhook/slackWebhook.service.js
+++ src/app/components/slackWebhook/slackWebhook.service.js
@@ -3,7 +3,7 @@ class SlackWebhookService{
         'ngInject';
 
         this.data = JSON.stringify({ text : 'There is a delivery.'});
-        this.slack = $resource('http://hooks.slack.com/services/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',
+        this.slack = $resource('https://hooks.slack.com/services/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',
             null,{
             delivery : {
                 method:'POST',
